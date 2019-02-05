c121ff62ac2527b634d21c6d829009f41a4a628b
Matheus Esteves <matheusesteveszanoto@gmail.com>
2019-02-04 23:10:16 -0200

    Refatoração para caminho da URL para API e customização do Admin para Model

--- agile_vision_project/agile_vision_app/admin.py
+++ agile_vision_project/agile_vision_app/admin.py
@@ -1,3 +1,8 @@
-from django.contrib import admin
+from django.contrib.admin import AdminSite
+from .models import User
 
-# Register your models here.
+class AgileVisionAdmin(AdminSite):
+    site_header = 'Agile Vision Administration'
+
+agilevision_admin_site = AgileVisionAdmin(name='admin')
+agilevision_admin_site.register(User)
